d665d41f8ee122562c0e41ef38388467cf7dfd98
icarz <mr.rghay@gmail.com>
2025-05-13 21:55:19 +0000

    task2

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,26 +1,17 @@
-// Import jQuery
-import $ from 'jquery';
+import $ from "jquery";
+import _ from "lodash";
 
-// Function to update the click counter
+$("body").append("<p>ALX Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Click here to get started</button>");
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - ALX School</p>");
+
 let count = 0;
-
 function updateCounter() {
-    count += 1;
-    $('#count').text(`${count} clicks on the button`);
+  count += 1;
+
+  $("#count").text(`${count} clicks on the button`);
 }
 
-// DOM manipulation using jQuery
-$(document).ready(function () {
-    // Add paragraphs and button to the body
-    $('body').append('<p>ALX Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<button id="startButton">Click here to get started</button>');
-    $('body').append('<p id="count"></p>');
-    $('body').append('<p>Copyright - ALX</p>');
-
-    // Bind updateCounter function to the button click event using debounce
-    const debouncedUpdateCounter = _.debounce(updateCounter, 500);  // Debounce the click event by 500ms
-
-    $('#startButton').on('click', debouncedUpdateCounter);
-});
-
+$("button").on("click", _.debounce(updateCounter, 500));
